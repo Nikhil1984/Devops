a21167a6fff2f61a691d725f85d469b1822ac564
SwissReDevops <59020369+SwissReDevops@users.noreply.github.com>
2020-02-20 21:44:47 +0530

    Update sap.groovy

--- sap.groovy
+++ sap.groovy
@@ -7,15 +7,32 @@ def abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE,OBJECT) {
 	
 	withCredentials([usernamePassword(credentialsId: 'NPL', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
 		stage( 'ABAP Unit Test') {
-			dir('sap-pipeline') {
-				bat "newman run abap_unit_coverage.postman_collection.json --insecure --bail " +
+			dir('sap-pipeline') { def count = 0
+					OBJECT.each{ def type = OBJECT[count].split( )
+					def prog = type[1].split('.asx.xml')	    
+			                if ( type[0] == 'DEVC' ){ PACKAGE = prog[0] 
+								  println "PACKAGE=" + PACKAGE 
+				bat "newman run abap_unit_coverage_pack.postman_collection.json --insecure --bail " +
 				"--environment NPL.postman_environment.json " +
 				"--timeout-request 120000 " +
 				"--global-var host=$HOST " +
 				"--global-var username=$USERNAME " +
 				"--global-var password=$PASSWORD " +
                  		"--global-var package=$PACKAGE " + 
-				"--global-var coverage_min=$COVERAGE "
+			        "--global-var coverage_min=$COVERAGE " }
+
+			        if ( type[0] == 'CLAS' ){ CLAS = prog[0]
+								  println "CLASS=" + CLAS 
+				bat "newman run abap_unit_coverage_clas.postman_collection.json --insecure --bail " +
+				"--environment NPL.postman_environment.json " +
+				"--timeout-request 120000 " +
+				"--global-var host=$HOST " +
+				"--global-var username=$USERNAME " +
+				"--global-var password=$PASSWORD " +
+                 		"--global-var clas=$CLAS " + 
+			        "--global-var coverage_min=$COVERAGE "} 
+			                count = count + 1
+
 			}
 		}
 	}
