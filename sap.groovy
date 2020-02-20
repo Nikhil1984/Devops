ff05298927e18ff5333f0982af7a7216985c7004
SwissReDevops <59020369+SwissReDevops@users.noreply.github.com>
2020-02-20 19:36:23 +0530

    Update sap.groovy

--- sap.groovy
+++ sap.groovy
@@ -32,7 +32,7 @@ def abap_sci(LABEL,HOST,CREDENTIAL,PACKAGE,VARIANT,OBJECT) {
 		stage('ABAP Code Inpector') {
 			dir('sap-pipeline') { def count = 0
 					OBJECT.each{ def type = OBJECT[count].split( )
-			                if ( type[0] == 'DEVC' ){ def prog = type[1].split( 'asx.xml')  $PACKAGE = prog[0] println $PACKAGE }
+			                if ( type[0] == 'DEVC' ){ def prog = type[1].split('asx.xml') PACKAGE = prog[0] println PACKAGE }
 					elseif ( type[0] == 'CLAS' ){  } 
 			                count = count + 1
 					bat "newman run abap_sci.postman_collection.json --insecure --bail " +
