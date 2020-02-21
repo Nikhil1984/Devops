139b0755af8c1d8e827485346a74b4a6c986d80f
SwissReDevops <59020369+SwissReDevops@users.noreply.github.com>
2020-02-21 15:07:42 +0530

    Update sap.groovy

--- sap.groovy
+++ sap.groovy
@@ -11,17 +11,6 @@ def abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE,OBJECT) {
 			dir('sap-pipeline') { def count = 0
 					OBJECT.each{ def type = OBJECT[count].split( )
 					def prog = type[1].split('.asx.xml')	    
-			                if ( type[0] == 'DEVC' ){ PACKAGE = prog[0] 
-								  println "PACKAGE=" + PACKAGE 
-				bat "newman run abap_unit_coverage_pack.postman_collection.json --insecure --bail " +
-				"--environment NPL.postman_environment.json " +
-				"--timeout-request 120000 " +
-				"--global-var host=$HOST " +
-				"--global-var username=$USERNAME " +
-				"--global-var password=$PASSWORD " +
-                 		"--global-var package=$PACKAGE " + 
-			        "--global-var coverage_min=$COVERAGE " }
-
 			        if ( type[0] == 'CLAS' ){ CLAS = prog[0]
 								  println "CLASS=" + CLAS 
 				bat "newman run abap_unit_coverage_clas.postman_collection.json --insecure --bail " +
@@ -33,7 +22,6 @@ def abap_unit(LABEL,HOST,CREDENTIAL,PACKAGE,COVERAGE,OBJECT) {
                  		"--global-var clas=$CLAS " + 
 			        "--global-var coverage_min=$COVERAGE " } 
 			                count = count + 1
-
 			}
 		}
 	}
